bce51d58abc1a04c6acf197f0e833bcda14916c6
zhangyongpu <zhangyongpu@meituan.com>
2018-08-27 00:44:23 +0800

    xjfa

--- src/java/Test.java
+++ src/java/Test.java
@@ -3,6 +3,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("I am zyp");
+        System.out.println("zhangyongpu love zhangqin");
     }
 
 }
